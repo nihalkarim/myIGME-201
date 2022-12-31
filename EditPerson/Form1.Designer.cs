31ce81221a360fc1935f0e8c505ee6414f6fea8f
Nihal Karim <nihalkarim@gmail.com>
2022-12-30 23:05:35 -0800

    pe21 + other stuff

---
+++ EditPerson/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace EditPerson
+{
+    partial class PersonEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.typeComboBox = new System.Windows.Forms.ComboBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameText = new System.Windows.Forms.TextBox();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.emailText = new System.Windows.Forms.TextBox();
+            this.ageText = new System.Windows.Forms.TextBox();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.Location = new System.Drawing.Point(9, 20);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(70, 13);
+            this.typeLabel.TabIndex = 0;
+            this.typeLabel.Text = "Person Type:";
+            // 
+            // typeComboBox
+            // 
+            this.typeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.typeComboBox.FormattingEnabled = true;
+            this.typeComboBox.Items.AddRange(new object[] {
+            "Student",
+            "Teacher"});
+            this.typeComboBox.Location = new System.Drawing.Point(80, 18);
+            this.typeComboBox.MaxDropDownItems = 2;
+            this.typeComboBox.Name = "typeComboBox";
+            this.typeComboBox.Size = new System.Drawing.Size(119, 24);
+            this.typeComboBox.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Location = new System.Drawing.Point(9, 55);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(70, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name:";
+            // 
+            // nameText
+            // 
+            this.nameText.Location = new System.Drawing.Point(80, 53);
+            this.nameText.Name = "nameText";
+            this.nameText.Size = new System.Drawing.Size(207, 22);
+            this.nameText.TabIndex = 1;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.Location = new System.Drawing.Point(9, 91);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(70, 13);
+            this.emailLabel.TabIndex = 2;
+            this.emailLabel.Text = "Email:";
+            // 
+            // emailText
+            // 
+            this.emailText.Location = new System.Drawing.Point(80, 88);
+            this.emailText.Name = "emailText";
+            this.emailText.Size = new System.Drawing.Size(352, 22);
+            this.emailText.TabIndex = 2;
+            // 
+            // ageText
+            // 
+            this.ageText.Location = new System.Drawing.Point(80, 128);
+            this.ageText.Name = "ageText";
+            this.ageText.Size = new System.Drawing.Size(70, 22);
+            this.ageText.TabIndex = 3;
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.Location = new System.Drawing.Point(9, 131);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(70, 13);
+            this.ageLabel.TabIndex = 4;
+            this.ageLabel.Text = "Age:";
+            // 
+            // PersonEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 268);
+            this.Controls.Add(this.ageText);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.emailText);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.nameText);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.typeComboBox);
+            this.Controls.Add(this.typeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(462, 319);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(462, 319);
+            this.Name = "PersonEditForm";
+            this.ShowInTaskbar = false;
+            this.Text = "EditPerson";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.ComboBox typeComboBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameText;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.TextBox emailText;
+        private System.Windows.Forms.TextBox ageText;
+        private System.Windows.Forms.Label ageLabel;
+    }
+}
+
